d7e1f4a4bac50336983c00eb563cdc745174c3d5
osmandemirel <song_osman@hotmail.com>
2020-12-13 13:07:24 +0300

    Update preprocess_data to process multiple folder at once

--- src/data/preprocess_data.py
+++ src/data/preprocess_data.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import tarfile
 
@@ -32,110 +33,109 @@ def load_challenge_data(header_file):
     return recording, header
 
 
-input_directory = "data/raw/Training_WFDB/"
+input_directory = "data/raw"
 output_directory = "data/processed"
-"""
-for f in os.listdir(input_directory)[:4]:
-    g = os.path.join(input_directory, f)
-    if not f.lower().startswith('.') and f.lower().endswith('hea') and os.path.isfile(g):
-        recording, header = load_challenge_data(g)
-"""
 
+# loop in raw data folders
+for input_folder in glob.glob(f"{input_directory}/*"):
+    folder_name = input_folder.split("/")[-1]
+    print(f"processing {folder_name} data..")
 
-header_files = []
-for f in os.listdir(input_directory):
-    g = os.path.join(input_directory, f)
-    if not f.lower().startswith('.') and f.lower().endswith('hea') and os.path.isfile(g):
-        header_files.append(g)
+    header_files = []
+    for f in os.listdir(input_folder):
+        g = os.path.join(input_folder, f)
+        if not f.lower().startswith('.') and f.lower().endswith('hea') and os.path.isfile(g):
+            header_files.append(g)
 
-classes = get_classes(input_directory, header_files)
-num_classes = len(classes)
-num_files = len(header_files)
-recordings = list()
-headers = list()
+    classes = get_classes(input_folder, header_files)
+    num_classes = len(classes)
+    num_files = len(header_files)
+    recordings = list()
+    headers = list()
 
-for i in range(num_files):
-    recording, header = load_challenge_data(header_files[i])
-    recordings.append(recording)
-    headers.append(header)
+    for i in range(num_files):
+        recording, header = load_challenge_data(header_files[i])
+        recordings.append(recording)
+        headers.append(header)
 
-features = list()
-labels = list()
+    features = list()
+    labels = list()
 
-for i in range(num_files):
-    recording = recordings[i]
-    header = headers[i]
+    for i in range(num_files):
+        recording = recordings[i]
+        header = headers[i]
 
-    tmp = get_12ECG_features(recording, header)
-    features.append(tmp)
+        tmp = get_12ECG_features(recording, header)
+        features.append(tmp)
 
-    for l in header:
-        if l.startswith('#Dx:'):
-            labels_act = np.zeros(num_classes)
-            arrs = l.strip().split(' ')
-            for arr in arrs[1].split(','):
-                class_index = classes.index(arr.rstrip()) # Only use first positive index
-                labels_act[class_index] = 1
-    labels.append(labels_act)
+        for l in header:
+            if l.startswith('#Dx:'):
+                labels_act = np.zeros(num_classes)
+                arrs = l.strip().split(' ')
+                for arr in arrs[1].split(','):
+                    class_index = classes.index(arr.rstrip()) # Only use first positive index
+                    labels_act[class_index] = 1
+        labels.append(labels_act)
 
-# "age", "sex", "mean_RR", "mean_Peaks", "median_RR", "median_Peaks", "std_RR", "std_Peaks", "var_RR", "var_Peaks", "skew_RR", "skew_Peaks", "kurt_RR", "kurt_Peaks"
-features = np.array(features)
-labels = np.array(labels)
+    # "age", "sex", "mean_RR", "mean_Peaks", "median_RR", "median_Peaks", "std_RR", "std_Peaks", "var_RR", "var_Peaks", "skew_RR", "skew_Peaks", "kurt_RR", "kurt_Peaks"
+    features = np.array(features)
+    labels = np.array(labels)
 
-# features
-# since number of feautes has not been determined we create them statically
-fields_features = [
-    ('age', pa.float32()),
-    ('sex', pa.float32()),
-    ('mean_RR', pa.float32()),
-    ('mean_Peaks', pa.float32()),
-    ('median_RR', pa.float32()),
-    ('median_Peaks', pa.float32()),
-    ('std_RR', pa.float32()),
-    ('std_Peaks', pa.float32()),
-    ('var_RR', pa.float32()),
-    ('var_Peaks', pa.float32()),
-    ('skew_RR', pa.float32()),
-    ('skew_Peaks', pa.float32()),
-    ('kurt_RR', pa.float32()),
-    ('kurt_Peaks', pa.float32()),
-]
-table_features_arrays = [
-    pa.array(features[:, 0]), 
-    pa.array(features[:, 1]), 
-    pa.array(features[:, 2]), 
-    pa.array(features[:, 3]), 
-    pa.array(features[:, 4]), 
-    pa.array(features[:, 5]), 
-    pa.array(features[:, 6]), 
-    pa.array(features[:, 7]), 
-    pa.array(features[:, 8]), 
-    pa.array(features[:, 9]), 
-    pa.array(features[:, 10]), 
-    pa.array(features[:, 11]), 
-    pa.array(features[:, 12]), 
-    pa.array(features[:, 13]),
-]
+    # features
+    # since number of feautes has not been determined we create them statically
+    fields_features = [
+        ('age', pa.float32()),
+        ('sex', pa.float32()),
+        ('mean_RR', pa.float32()),
+        ('mean_Peaks', pa.float32()),
+        ('median_RR', pa.float32()),
+        ('median_Peaks', pa.float32()),
+        ('std_RR', pa.float32()),
+        ('std_Peaks', pa.float32()),
+        ('var_RR', pa.float32()),
+        ('var_Peaks', pa.float32()),
+        ('skew_RR', pa.float32()),
+        ('skew_Peaks', pa.float32()),
+        ('kurt_RR', pa.float32()),
+        ('kurt_Peaks', pa.float32()),
+    ]
+    table_features_arrays = [
+        pa.array(features[:, 0]), 
+        pa.array(features[:, 1]), 
+        pa.array(features[:, 2]), 
+        pa.array(features[:, 3]), 
+        pa.array(features[:, 4]), 
+        pa.array(features[:, 5]), 
+        pa.array(features[:, 6]), 
+        pa.array(features[:, 7]), 
+        pa.array(features[:, 8]), 
+        pa.array(features[:, 9]), 
+        pa.array(features[:, 10]), 
+        pa.array(features[:, 11]), 
+        pa.array(features[:, 12]), 
+        pa.array(features[:, 13]),
+    ]
 
-# labels
-# with loop we get label fields and values dynamically
-fields_labels = []
-table_labels_arrays = []
-for l in range(num_classes):
-    fields_labels += (f'label_{l+1}', pa.int8()),
-    table_labels_arrays += pa.array(labels[:, l]),
+    # labels
+    # with loop we get label fields and values dynamically
+    fields_labels = []
+    table_labels_arrays = []
+    for l in range(num_classes):
+        fields_labels += (f'label_{l+1}', pa.int8()),
+        table_labels_arrays += pa.array(labels[:, l]),
 
-# concat features and labels
-fields = fields_features + fields_labels
-table_arrays = table_features_arrays + table_labels_arrays
+    # concat features and labels
+    fields = fields_features + fields_labels
+    table_arrays = table_features_arrays + table_labels_arrays
 
-# create parquet objects
-schema = pa.schema(fields)
-table = pa.Table.from_arrays(
-    table_arrays,
-    schema=schema,
-    )
+    # create parquet objects
+    schema = pa.schema(fields)
+    table = pa.Table.from_arrays(
+        table_arrays,
+        schema=schema,
+        )
 
-# write concated data to parquet
-output_path_labels = os.path.join(output_directory, "processed.parquet")
-pq.write_table(table, where=output_path_labels)
+    print(f"writing {folder_name} data..")
+    # write concated data to parquet
+    output_path_labels = os.path.join(output_directory, f"processed_{folder_name}.parquet")
+    pq.write_table(table, where=output_path_labels)
