019da3e680d79516552bf5b4f1a3604cf3002c09
dorukuzucu <doruk.kuzucu@accenture.com>
2020-12-17 11:59:00 +0300

    refactoring for training

--- src/model/train.py
+++ src/model/train.py
@@ -1,16 +1,10 @@
-from collections import OrderedDict
-import torch
-from petastorm import make_batch_reader
-from petastorm.pytorch import DataLoader
-from torch.autograd import Variable
-from src.model.utils import create_dict_combination, dict_to_torch
-from src.data.losses import *
+from src.model.utils import create_dict_combination, dict_to_torch, correct_predictions
+from models.losses import *
 from models.dummy_model import TestNet
 from src.data.data_loader import ECGParquetDataloader
-import numpy as np
-import torch.nn.functional as F
 import torch.nn as nn
 import torch.optim as optim
+
 
 # TODO set a method for epoch train
 # TODO begin_run, begin_epoch methods
@@ -29,79 +23,96 @@ class TrainManager:
     def train(self):
         for run in self.runs:
             # set optimizer and loss function for this run
-            self.set_optimizer(learning_rate=run.learning_rate, optim_type=run.optimizer, weight_decay=run.weight_decay,momentum=run.momentum)
-            self.set_criterion(run.loss_fn)
+            self.begin_run(run=run)
 
             # iterate over epochs
             for epoch in range(run.epochs):
                 # get dataset for this epoch to be trained on
                 self.dataset = self.train_loader.new_loader(num_epochs=1, batch_size=run.batch_size)
-                self.set_criterion(run.loss_fn)
+
+                # to calculate accuracy each epoch
+                correct_prediction_count = 0
+                total_predictions = 0
 
                 # iterate over batches
                 epoch_loss = 0
                 for batch_data in self.dataset:
-                    # we need to proprocess parquet data it order to feed it to network
-                    features, labels = dict_to_torch(batch_data, batch_size=run.batch_size, feature_count=14)
-                    predictions = self.model(features.float())
+                    # we need to preprocess parquet data it order to feed it to network
+                    features, labels = dict_to_torch(batch_data, feature_count=14)
+                    predictions = self.model(features)
 
                     # calculate loss and update weights for batch
-                    loss = self.criterion(predictions, labels)
+                    loss_out = self.criterion(predictions, labels)
                     self.optimizer.zero_grad()
-                    loss.backward()
+                    loss_out.backward()
                     self.optimizer.step()
-                    epoch_loss += loss
-                print("Epoch:{} Loss:{}".format(epoch, epoch_loss))
+                    epoch_loss += loss_out
+
+                    # add correct and total predictions
+                    correct_prediction_count += correct_predictions(predictions, labels)
+                    total_predictions += run.batch_size
+                print("Epoch:{} Loss:{} Accuracy:{}".format(epoch, epoch_loss, correct_prediction_count/total_predictions*100))
                 """
                 Validation epoch, every x epoch decided by user, we will validate our model
                 """
                 if epoch % run.epochs_for_val == 0:
                     print("Validating model")
                     self.dataset = self.val_loader.new_loader(num_epochs=1, batch_size=run.batch_size)
+
+                    # to calculate metrics each epoch
+                    correct_prediction_count = 0
+                    total_predictions = 0
                     val_loss = 0
                     for batch_data in self.dataset:
+                        # we need to preprocess parquet data it order to feed it to network
                         features, labels = dict_to_torch(batch_data, batch_size=run.batch_size, feature_count=14)
                         predictions = self.model(features.float())
+                        # calculate loss for batch
                         criterion = nn.BCEWithLogitsLoss(pos_weight=self.model.loss_weights)
-                        loss = criterion(predictions, labels.float())
-                        val_loss += loss
-                    print("Validation Epoch:{} Loss:{}".format(epoch, val_loss))
+                        loss_out = criterion(predictions, labels.float())
+                        val_loss += loss_out
+                        # add correct and total predictions
+                        correct_prediction_count += correct_predictions(predictions, labels)
+                        total_predictions += run.batch_size
+                    print("Validation Epoch:{} Loss:{} Accuracy:{}".format(epoch, epoch_loss, correct_prediction_count/total_predictions*100))
 
-    def set_optimizer(self, optim_type, learning_rate, weight_decay, momentum):
-        if optim_type == "Adam":
-            self.optimizer = optim.Adam(params=self.model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
-        elif optim_type == "SGD":
-            self.optimizer = optim.SGD(params=self.model.parameters(), lr=learning_rate, weight_decay=weight_decay, momentum=momentum)
-        else:
-            print("Setting for default optimizer")
-            self.optimizer = optim.Adam(params=self.model.parameters(),lr=learning_rate)
+    def set_optimizer(self, run):
+        if run.optimizer_type == "Adam":
+            self.optimizer = optim.Adam(params=self.model.parameters(), lr=run.learning_rate, weight_decay=run.weight_decay)
+        elif run.optimizer_type == "SGD":
+            self.optimizer = optim.SGD(params=self.model.parameters(), lr=run.learning_rate, weight_decay=run.weight_decay,
+                                       momentum=run.momentum)
 
-    def set_criterion(self,loss):
-        if loss == "dice":
-            pass
-        elif loss == "PenaltyDice":
-            pass
-        elif loss == "PenaltyBCE":
-            pass
-        else:
-            self.criterion = nn.BCEWithLogitsLoss(pos_weight=self.model.loss_weights)
+    def set_criterion(self, loss_fn="penalty_dice"):
+        if loss_fn == "dice":
+            self.criterion = SoftDiceLoss()
+        elif loss_fn == "penalty_l1":
+            self.criterion = L1LossWithPenalty()
+        elif loss_fn == "penalty_mse":
+            self.criterion = MSELossWithPenalty()
+        elif loss_fn == "penalty_dice":
+            self.criterion = SoftDiceLossWithPenalty()
 
+    def begin_run(self, run):
+        self.set_optimizer(run)
+        self.set_criterion(run.loss_fn)
 
 
 dummy_model = TestNet()
 data_path = r'file:C:\Users\ABRA\Desktop\Ders\Yüksek Lisans\BLG561-Deep Learning\deep_learning_interim_project\data\processed'
+data_path_1 = r'file:C:/Users/ABRA/Desktop/Ders/Yüksek Lisans/BLG561-Deep Learning/deep_learning_interim_project/data/processed'
 
 dummy_params = {
     'learning_rate': [0.01],
     'batch_size': [3],
     'epochs': [100],
     'num_workers': [0],
-    'optimizer': ["Adam"],
+    'optimizer_type': ["Adam"],
     'loss_fn': ["Test"],
     'epochs_for_val': [4],
     'weight_decay': [1e-4],
     'momentum': [0]
 }
 
-mngr = TrainManager(model=dummy_model, processed_data_path=loader, training_config=dummy_params)
+mngr = TrainManager(model=dummy_model, processed_data_path=data_path_1, training_config=dummy_params)
 mngr.train()
--- src/model/utils.py
+++ src/model/utils.py
@@ -10,7 +10,7 @@ def create_dict_combination(dict):
     ordered_dict = OrderedDict(dict)
     combinations = product(*ordered_dict.values())
     # create named tuple to save combinations
-    combination_tuple = namedtuple('run',ordered_dict.keys())
+    combination_tuple = namedtuple('run', ordered_dict.keys())
     # create array to save namedtupled and iterate over them
     run_configs = []
 
@@ -21,7 +21,24 @@ def create_dict_combination(dict):
     # return value
     return run_configs
 
-def dict_to_torch(dict, feature_count, batch_size):
-    features = [value.tolist() for value in list(dict.values())[:feature_count]]
-    labels = [value.tolist() for value in list(dict.values())[feature_count:]]
-    return torch.from_numpy(np.array(features).T), torch.from_numpy(np.array(labels).T).float()
+
+def dict_to_torch(dict_inp, feature_count):
+    features = [value.tolist() for value in list(dict_inp.values())[:feature_count]]
+    labels = [value.tolist() for value in list(dict_inp.values())[feature_count:]]
+    return torch.from_numpy(np.array(features).T).float(), torch.from_numpy(np.array(labels).T).float()
+
+
+def correct_predictions(predictions, targets):
+    """
+    :param predictions: input of predicted values. size should be WxC
+    :param targets: input of target values. size should be WxC
+    :return: total number pf correct predictions
+    """
+    # shapes should be the same
+    assert predictions.size() == targets.size()
+    # calculate correct predictions over each batch
+    correct = 0
+    for batch in range(predictions.size(0)):
+        if torch.equal(predictions[batch], targets[batch]):
+            correct += 1
+    return correct
